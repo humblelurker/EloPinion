8084f42e3ac719f428f8323bd9759b1aa63937a2
humblelurker <164403702+humblelurker@users.noreply.github.com>
2025-05-05 18:52:28 -0400

    a

--- backend/urls.py
+++ backend/urls.py
@@ -14,9 +14,12 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
 from django.urls import path
+from django.http import JsonResponse
+
+def hello(request):
+	return JsonResponse({"message": "ola desde Django"})
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+	path("api/hello/", hello),
 ]
